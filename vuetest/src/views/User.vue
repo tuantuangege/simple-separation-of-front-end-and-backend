7f518b20bdb17fd0d8542735b52e58f29e44014a
团团哥哥 <7544971+tuantuangege@user.noreply.gitee.com>
2020-09-14 19:04:25 +0800

    前后端分离前端内容

---
+++ vuetest/src/views/User.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>用户名</td>
+                <td>密码</td>
+            </tr>
+            <tr v-for="user in users">
+                <td>{{user.id}}</td>
+                <td>{{user.username}}</td>
+                <td>{{user.password}}</td>
+            </tr>
+        </table>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "User",
+        data(){
+            return {
+                msg:"hello,vue",
+                users:[{
+                    id:1,
+                    username:'tom',
+                    password:'cat'
+                },{
+                    id:2,
+                    username:'tom2',
+                    password:'cat2'
+                }
+                ]
+            }
+        },created() {
+            const _this=this
+            axios.get('http://localhost:8080/user/findAll').then(function (resp) {
+                    _this.users=resp.data
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
